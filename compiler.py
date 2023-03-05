5672b6bf91843524f122dfd65f547edaa2828209
jarvisar <jarvisar@mail.uc.edu>
2023-03-04 23:51:40 -0500

    Fixed issue with subtraction and addition

--- compiler.py
+++ compiler.py
@@ -126,73 +126,76 @@ class Num(AST):
 
 
 class Parser:
-	def __init__(self, lexer):
-		self.lexer = lexer
-		self.current_token = self.lexer.get_next_token()
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
 
-	def error(self):
-		raise Exception('Invalid syntax')
+    def error(self):
+        raise Exception('Invalid syntax')
 
-	def eat(self, token_type):
-		if self.current_token.type == token_type:
-			self.current_token = self.lexer.get_next_token()
-		else:
-			self.error()
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
 
-	def factor(self):
-		token = self.current_token
-		if token.type == INTEGER:
-			self.eat(INTEGER)
-			return Num(token)
-		elif token.type == LPAREN:
-			self.eat(LPAREN)
-			node = self.expr()
-			self.eat(RPAREN)
-			return node
-		elif token.type == EXPONENTIATION:
-			self.eat(EXPONENTIATION)
-			node = self.factor()
-			return BinOp(left=Num(Token(INTEGER, '2')), op=EXPONENTIATION, right=node)
-		elif token.type == MODULO:
-			self.eat(MODULO)
-			node = BinOp(left=node, op=token, right=self.factor())
-			return node
-		else:
-			self.error()
+    def factor(self):
+        token = self.current_token
+        if token.type == INTEGER:
+            self.eat(INTEGER)
+            return Num(token)
+        elif token.type == LPAREN:
+            self.eat(LPAREN)
+            node = self.expr()
+            self.eat(RPAREN)
+            return node
+        elif token.type == EXPONENTIATION:
+            self.eat(EXPONENTIATION)
+            node = self.factor()
+            return BinOp(left=Num(Token(INTEGER, '2')), op=EXPONENTIATION, right=node)
+        elif token.type == MODULO:
+            self.eat(MODULO)
+            node = BinOp(left=node, op=token, right=self.factor())
+            return node
+        else:
+            self.error()
 
-	def term(self):
-		node = self.factor()
+    def term(self):
+        node = self.factor()
 
-		while self.current_token.type in (MULTIPLY, DIVIDE, FLOOR_DIVIDE, EXPONENTIATION, MODULO):
-			token = self.current_token
-			if token.type == MULTIPLY:
-				self.eat(MULTIPLY)
-			elif token.type == DIVIDE:
-				self.eat(DIVIDE)
-			elif token.type == FLOOR_DIVIDE:
-				self.eat(FLOOR_DIVIDE)
-			elif token.type == EXPONENTIATION:
-				self.eat(EXPONENTIATION)
-			elif token.type == MODULO:
-				self.eat(MODULO)
-		node = BinOp(left=node, op=token, right=self.factor())
+        while self.current_token.type in (MULTIPLY, DIVIDE, FLOOR_DIVIDE, EXPONENTIATION, MODULO):
+            token = self.current_token
+            if token.type == MULTIPLY:
+                self.eat(MULTIPLY)
+                node = BinOp(left=node, op=token, right=self.factor())
+            elif token.type == DIVIDE:
+                self.eat(DIVIDE)
+                node = BinOp(left=node, op=token, right=self.factor())
+            elif token.type == FLOOR_DIVIDE:
+                self.eat(FLOOR_DIVIDE)
+                node = BinOp(left=node, op=token, right=self.factor())
+            elif token.type == EXPONENTIATION:
+                self.eat(EXPONENTIATION)
+                node = BinOp(left=node, op=token, right=self.factor())
+            elif token.type == MODULO:
+                self.eat(MODULO)
+                node = BinOp(left=node, op=token, right=self.factor())
 
-		return node
+        return node
 
-	def expr(self):
-		node = self.term()
+    def expr(self):
+        node = self.term()
 
-		while self.current_token.type in (PLUS, MINUS):
-			token = self.current_token
-			if token.type == PLUS:
-				self.eat(PLUS)
-			elif token.type == MINUS:
-				self.eat(MINUS)
+        while self.current_token.type in (PLUS, MINUS):
+            token = self.current_token
+            if token.type == PLUS:
+                self.eat(PLUS)
+            elif token.type == MINUS:
+                self.eat(MINUS)
 
-			node = BinOp(left=node, op=token, right=self.term())
+            node = BinOp(left=node, op=token, right=self.term())
 
-		return node
-
+        return node
 
 
 class Interpreter:
