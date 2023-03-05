d9d39097c25f84e349a121ab6a031f2b4cf0d2ee
jarvisar <jarvisar@mail.uc.edu>
2023-03-05 11:09:32 -0500

    Comment out extra info

--- interpreter.py
+++ interpreter.py
@@ -254,8 +254,8 @@ class Interpreter:
         tree = self.parser.expr()
         return self.visit(tree)
 
-print("Currently supports addition (+), subtraction (-), multiplication (*), division (/), exponents (**), floor division (//), and modulus (%).")
-print("Works with integers and decimals and supports parentheses, e.g. '(3 + 4) / 5' outputs 1.4 and '3 + 4 / 5' outputs 3.8.")
+# print("Currently supports addition (+), subtraction (-), multiplication (*), division (/), exponents (**), floor division (//), and modulus (%).")
+# print("Works with integers and decimals and supports parentheses, e.g. '(3 + 4) / 5' outputs 1.4 and '3 + 4 / 5' outputs 3.8.")
 while True:
     text = input("Enter an arithmetic expression: ")
     lexer = Lexer(text)
