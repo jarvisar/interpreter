a8025a019948e5de41b8d15a2a8e8fc6a67c224b
jarvisar <jarvisar@mail.uc.edu>
2023-03-06 21:21:37 -0500

    Fix sqrt assembly for perfect squares

--- generator.py
+++ generator.py
@@ -58,9 +58,9 @@ class CodeGenerator:
     def visit_FuncCall(self, node: FuncCall):
         self.visit(node.arg)
         if node.func == "sqrt":
-            self.result.append("movq %rax, %xmm0")
+            self.result.append("cvtsi2sd %rax, %xmm0")
             self.result.append("sqrtsd %xmm0, %xmm0")
-            self.result.append("movq %xmm0, %rax")
+            self.result.append("cvtsd2si %xmm0, %rsi")
         elif node.func == "sin":
             self.result.append("movq %rax, %xmm0")
             self.result.append("call sin")
