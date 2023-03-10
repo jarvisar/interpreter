2450814bbcb2fdef806bc0c0758c816bee37f0f1
jarvisar <jarvisar@mail.uc.edu>
2023-03-09 23:10:26 -0500

    Fixed issue with binops in semantic analyzer

--- analyzer.py
+++ analyzer.py
@@ -12,18 +12,26 @@ class SemanticAnalyzer:
             self.analyze(node.right)
             if node.left.type != node.right.type:
                 raise Exception(f"Type mismatch between {node.left.type} and {node.right.type}")
+
             node.type = node.left.type
+            
         elif isinstance(node, Num):
             node.type = "int"
+
         elif isinstance(node, FuncCall):
             self.analyze(node.arg)
+
             if node.func not in self.symbol_table:
                 raise Exception(f"Function {node.func} is not defined")
+
             func_return_type = self.symbol_table[node.func]
             arg_type = node.arg.type
+
             if arg_type != func_return_type.arg_type:
                 raise Exception(f"Type mismatch between argument {arg_type} and function {func_return_type.arg_type}")
+
             node.type = func_return_type.return_type
+
         elif isinstance(node, UnaryOp):
             self.analyze(node.expr)
             node.type = node.expr.type
