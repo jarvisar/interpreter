6fa84fd17c90b020b834794c8db6615350d9ac0d
jarvisar <jarvisar@mail.uc.edu>
2023-03-11 09:56:05 -0500

    Can directly execute assembly code

--- assembly.s
+++ assembly.s
@@ -7,29 +7,29 @@ result_fmt: .string "Result: %ld\n"
 main:
     subq $8, %rsp
     # Calculate expression (paste generated assembly code here)
-movq $8, %rax
-pushq %rax
-movq $9000, %rax
-pushq %rax
-movq $2, %rax
-movq %rax, %rbx
-popq %rax
-movq %rax, %rcx
-movq $1, %rax
-cmpq $0, %rbx
-je .exponentiation_1_done
-.exponentiation_1_loop:
-testq $1, %rbx
-jz .exponentiation_1_square
-imulq %rcx, %rax
-.exponentiation_1_square:
-imulq %rcx, %rcx
-shr $1, %rbx
-jnz .exponentiation_1_loop
-.exponentiation_1_done:
-movq %rax, %rbx
-popq %rax
-addq %rbx, %rax
+    movq $8, %rax
+    pushq %rax
+    movq $9000, %rax
+    pushq %rax
+    movq $2, %rax
+    movq %rax, %rbx
+    popq %rax
+    movq %rax, %rcx
+    movq $1, %rax
+    cmpq $0, %rbx
+    je .exponentiation_1_done
+    .exponentiation_1_loop:
+    testq $1, %rbx
+    jz .exponentiation_1_square
+    imulq %rcx, %rax
+    .exponentiation_1_square:
+    imulq %rcx, %rcx
+    shr $1, %rbx
+    jnz .exponentiation_1_loop
+    .exponentiation_1_done:
+    movq %rax, %rbx
+    popq %rax
+    addq %rbx, %rax
     # End of calculation
 
     # Print the result
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from lexer import Lexer
 from interpreter import Interpreter
 from generator import CodeGenerator
 from analyzer import SemanticAnalyzer
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", help="input file name")
@@ -37,9 +38,29 @@ if args.file:
                     if text.endswith(";"):
                         generator = CodeGenerator(parser, symbol_table)
                         assembly_code = generator.generate_code()
-                        print("== Begin Assembly Code ==")
-                        print("\n".join(assembly_code))
-                        print("== End Assembly Code ==")
+                        with open("assembly.s", "w") as f:
+                            f.write(".section .data\n")
+                            f.write("result_fmt: .string \"Result: %ld\\n\"\n")
+                            f.write("\n")
+                            f.write(".section .text\n")
+                            f.write(".globl main\n")
+                            f.write(".type main, @function\n")
+                            f.write("main:\n")
+                            f.write("    subq $8, %rsp\n")
+                            f.write("    # Calculate expression (paste generated assembly code here)\n")
+                            f.write("\n".join(assembly_code) + "\n")
+                            f.write("    # End of calculation\n")
+                            f.write("\n")
+                            f.write("    # Print the result\n")
+                            f.write("    movq %rax, %rsi\n")
+                            f.write("    movq $result_fmt, %rdi\n")
+                            f.write("    movq $0, %rax\n")
+                            f.write("    call printf\n")
+                            f.write("\n")
+                            f.write("    # Exit\n")
+                            f.write("    addq $8, %rsp # restore stack\n")
+                            f.write("    movq $0, %rax\n")
+                            f.write("    retq\n")
                     else:
                         interpreter = Interpreter(parser, symbol_table)
                         result = interpreter.interpret()
@@ -85,3 +106,28 @@ else:
                         print(f'Result: {result}')
         except Exception as e:
             print(f"Error: {e}")
+
+# Use the modified Python code to assemble, link, and run the generated code
+if os.path.exists('assembly.s'):
+    import platform
+    import subprocess
+
+    # Check if running on Windows
+    if platform.system() == 'Windows':
+        # Use WSL to run the commands
+        command_prefix = ['wsl', 'bash', '-c']
+        # Assemble the code
+        subprocess.run(command_prefix + ['as -o assembly.o assembly.s --64 -g'])
+        # Link the code
+        subprocess.run(command_prefix + ['gcc -shared -o assembly assembly.o -lm -no-pie -g'])
+        # Run the executable
+        subprocess.run(command_prefix + ['./assembly'])
+    else:
+        command_prefix = []
+        # Assemble the code
+        subprocess.run(command_prefix + ['gcc', '-c', '-o', 'assembly.o', 'assembly.s', '--64'])
+        # Link the code
+        subprocess.run(command_prefix + ['gcc', '-o', 'assembly', 'assembly.o', '-lm', '-no-pie'])
+        # Run the executable
+        subprocess.run(command_prefix + ['./assembly'])
+    
