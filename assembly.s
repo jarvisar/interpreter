d8f292517b4266987a656100d7267bdbfdb8cc0d
Adam Jarvis <55147029+jarvisar@users.noreply.github.com>
2023-04-27 00:40:39 -0400

    Update assembly.s

--- assembly.s
+++ assembly.s
@@ -1,19 +1,59 @@
-const   dd 1000.0
 .section .data
-result_fmt: .string "Result: %.1f\n"
+result_fmt: .string "Result: %ld\n"
 
 .section .text
 .globl main
 .type main, @function
 main:
-subq $8, %rsp
-movss xmm0,[const]
-movq $2, %rax
-movq %rax, %xmm1
-addsd %xmm1, %xmm0
-movq %xmm0, %rdi
-movq $result_fmt, %rax
-call printf
-addq $8, %rsp
-xorq %rax, %rax
-retq
+    subq $8, %rsp
+    # Calculate expression (paste generated assembly code here)
+    # Example input: (2 ^ 2)! > sqrt(625)
+    movq $2, %rax
+    pushq %rax
+    movq $2, %rax
+    movq %rax, %rbx
+    popq %rax
+    movq %rax, %rcx
+    movq $1, %rax
+    cmpq $0, %rbx
+    je .exponentiation_2_done
+    .exponentiation_2_loop:
+    testq $1, %rbx
+    jz .exponentiation_2_square
+    imulq %rcx, %rax
+    .exponentiation_2_square:
+    imulq %rcx, %rcx
+    shr $1, %rbx
+    jnz .exponentiation_2_loop
+    .exponentiation_2_done:
+    movq %rax, %rcx
+    movq $1, %rax
+    cmpq $0, %rcx
+    je .factorial_1_done
+    .factorial_1_loop:
+    imulq %rcx, %rax
+    decq %rcx
+    jnz .factorial_1_loop
+    .factorial_1_done:
+    pushq %rax
+    movq $625, %rax
+    cvtsi2sd %rax, %xmm0
+    sqrtsd %xmm0, %xmm0
+    cvttsd2si %xmm0, %rax
+    movq %rax, %rbx
+    popq %rax
+    cmpq %rbx, %rax
+    setg %al
+    movzbq %al, %rax
+    # End of calculation
+
+    # Print the result
+    movq %rax, %rsi
+    movq $result_fmt, %rdi
+    movq $0, %rax
+    call printf
+
+    # Exit
+    addq $8, %rsp # restore stack
+    movq $0, %rax
+    retq
